39c0c0f3b55eb9aec7c3b569dd3b8e8dc6f529eb
Teng Li <lite@flip1.engr.oregonstate.edu>
2017-10-30 23:50:43 -0700

    add check user function & find user_id by onid

--- pages/actions/assign_question.php
+++ pages/actions/assign_question.php
@@ -1,5 +1,34 @@
 <?php
-	require('../db_config/conn.php');
+	require_once ('../db_config/conn.php');
+	include_once ('check_user.php');
+
+	$question_id = $_REQUEST['question_id'];
+	// Can not get question_id from font page, exit! 
+	if(!isset($question_id)){
+		exit(json_encode(array('ERROR'=>'Failed to get question_id!')));
+	}
+
+	$onid = $_SESSION['onidid'];
+	$firstname = $_SESSION['firstname'];
+	$lastname = $_SESSION['lastname'];
+
+	if(!isset($onid) || !isset($firstname) || !isset($lastname)){
+		exit(json_encode(array('ERROR'=>'Cannot get user information!')));
+	}
+
+	//check the user is ta or not, only ta can assign question, role of ta is 1
+	$role = 1;
+	
+	$user_info = checkUserByQuestion($onid, $question_id, $role);
+	if($user_info){
+		if($firstname == $user_info['class_info']->first_name && $lastname == $user_info['class_info']->last_name){
+			$user_id = $user_info['class_info']->id;
+		} else {
+			exit(json_encode(array('ERROR'=>'User information is error!')));
+		}
+	} else {
+		exit(json_encode(array('ERROR'=>'User is not a TA in this class!')));
+	}
 
 	//parpare sql
 	function buildSql(){
@@ -16,20 +45,6 @@
 		return $sql;
 	}
 	
-	$question_id = $_REQUEST['question_id'];
-	// Can not get question_id from font page, exit! 
-	if(!isset($question_id)){
-		exit(json_encode(array('ERROR'=>'Failed to get question_id!')));
-	}
-
-	$user_id = $_SESSION['user_id'];
-	$firstname = $_SESSION['firstname'];
-	$lastname = $_SESSION['lastname'];
-
-	if(!isset($user_id) || !isset($firstname) || !isset($lastname)){
-		exit(json_encode(array('ERROR'=>'Cannot get unser information!')));
-	}
-
 	$sql = buildSql();
 
 	// Preparing sql is failed, exit! 
@@ -41,6 +56,6 @@
 	$stmt->bind_param("issi", $user_id, $firstname, $lastname, $question_id);
 	$stmt->execute();
 
-	echo json_encode(array('Success'=>'Status update successfully!'));
+	echo json_encode(array('SUCCESS'=>'Status update successfully!'));
 	$mysqli->close();
 ?>
