391b298bf663a3c3f52f10a6baf98d11447f5cfe
DeqingQu <qu.deqing@gmail.com>
2017-11-05 08:20:14 -0800

    add js test framework to test folder

--- tests/js_test/test_sanity.js
+++ tests/js_test/test_sanity.js
@@ -1,7 +1,8 @@
-const expect = require('chai').expect;
+var expect = chai.expect;
+var should = chai.should();
 
 describe("sanity check", () => {
-    
+
     it("does math ok", (done) => {
         expect(1 + 1).to.equal(2);
         expect(1 + 1).not.to.equal(1);
