0689545b1829b51487f9dedd5433ad885d5861f2
Shannon Ernst <ernstsh@oregonstate.edu>
2017-11-06 06:41:15 -0800

    logic for getting input on TAs

--- pages/js/adminDashboard.js
+++ pages/js/adminDashboard.js
@@ -1,5 +1,26 @@
 function createCourse () {
+	function createCourse () {
+	var courseObj = {};
+	courseObj.courseName = document.getElementsByName("NAME")[0].value;
+	courseTAs = document.getElementsByName("TAS")[0].value;
+	courseObj.courseTAs = courseTAs.split(" ");
+	console.log(courseObj);
+	var str = JSON.stringify(courseObj);
+	str = "x=" + encodeURIComponent(str);
+	xmlhttp = new XMLHttpRequest();
+	xmlhttp.open("POST", "actions/createCourse.php", true);
+	xmlhttp.onreadystatechange = function () {
+		if(xmlhttp.readyState == 4){
+			if(xmlhttp.status == 200){
+				
+			}
+		}
+	}
+	xmlhttp.setRequestHeader("content-type", "application/x-www-form-urlencoded");
+	xmlhttp.send(str);
 	
+}
+
 }
 
 
