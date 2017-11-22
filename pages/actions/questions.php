0fdd819d0aacffd48cef612facd2b829210cb986
DeqingQu <qu.deqing@gmail.com>
2017-11-22 15:13:35 -0800

    modify interface of private methods

--- pages/actions/questions.php
+++ pages/actions/questions.php
@@ -9,41 +9,45 @@
 	{
 		private static $method_type = array('get', 'post', 'put', 'patch', 'delete');
 
-		public static function getRequest($mysqli) {
+		public static function getRequest($method) {
 			//	request method
-	        $method = strtolower($_SERVER['REQUEST_METHOD']);
 	        if (in_array($method, self::$method_type)) {
 		        //	call request method
 	            $data_name = $method . 'Question';
-	            return self::$data_name($_REQUEST, $mysqli);
+	            return self::$data_name($_REQUEST);
 			}
 	        return false;
 		}
 
 		//	GET
-		private static function getQuestion($request_data, $mysqli)
+		private static function getQuestion($request_data)
 		{
+			return self::generateData(400, "error", "Not Implemented");
 		}
 
 		//	POST
-		private static function postQuestion($request_data, $mysqli)
+		private static function postQuestion($request_data)
 		{
+			return self::generateData(400, "error", "Not Implemented");
 		}
 
 		//	PUT
-		private static function putQuestion($request_data, $mysqli)
+		private static function putQuestion($request_data)
 		{
+			return self::generateData(400, "error", "Not Implemented");
 		}
 
 		//	PATCH
-		private static function patchQuestion($request_data, $mysqli)
+		private static function patchQuestion($request_data)
 		{
+			return self::generateData(400, "error", "Not Implemented");
 		}
 
 		//	DELETE
 		//	questions.php/{question_id}
-		private static function deleteQuestion($request_data, $mysqli)
+		private static function deleteQuestion($request_data)
 		{
+			global $mysqli;
 
 			$url = $_SERVER['REQUEST_URI'];
 			$question_id = self::getQuestionID($url);
@@ -99,6 +103,9 @@
 				}
 				else {
 					$question_id = substr($url, $index, strlen($url)-$index);
+				}
+				if(intval($question_id) === 0 && substr($question_id, 0, 1) !== "0") {
+					return null;
 				}
 				return intval($question_id);
 			}
@@ -171,51 +178,7 @@
 		}
 	}
 
-
-	$data = QuestionRequest::getRequest($mysqli);
+	$method = strtolower($_SERVER['REQUEST_METHOD']);
+	$data = QuestionRequest::getRequest($method);
 	QuestionResponse::sendResponse($data);
-
-	// $onid = $_SESSION['onidid'];
-	// $firstname = $_SESSION['firstname'];
-	// $lastname = $_SESSION['lastname'];
-    //
-	// if(!isset($onid) || !isset($firstname) || !isset($lastname)){
-	// 	exit(json_encode(array('ERROR'=>'Cannot get user information!')));
-	// }
-
-	// check the user is ta or not, only ta can assign question, role of ta is 1
-	// $role = 1;
-    //
-	// $user_info = checkUserByQuestion($onid, $question_id, $role);
-	// if($user_info){
-	// 	if($firstname == $user_info['user_info']->first_name && $lastname == $user_info['user_info']->last_name){
-	// 		$user_id = $user_info['user_info']->id;
-	// 	} else {
-	// 		exit(json_encode(array('ERROR'=>'User information is error!')));
-	// 	}
-	// } else {
-	// 	exit(json_encode(array('ERROR'=>'User is not a TA in this class!')));
-	// }
-
-	// //parpare sql
-	// function buildSql(){
-	// 	$sql="DELETE FROM `t_question` q WHERE q.id = ? ";
-    //
-	// 	return $sql;
-	// }
-    //
-	// $sql = buildSql();
-    //
-	// // Preparing sql is failed, exit!
-	// if(!isset($sql)){
-	// 	exit(json_encode(array('ERROR'=>'Failed to Prepare sql!')));
-	// }
-    //
-	// $stmt = $mysqli->prepare($sql);
-	// $stmt->bind_param("issi", $user_id, $firstname, $lastname, $question_id);
-	// $stmt->execute();
-
-	// echo json_encode(array('SUCCESS'=>'Status update successfully!'));
-	// $mysqli->close();
-
 ?>
