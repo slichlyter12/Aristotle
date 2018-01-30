909b0db8ed0bc376b5104187c35e631d75ff3357
DeqingQu <qu.deqing@gmail.com>
2018-01-30 15:55:20 -0800

    use Unittest in python to assert

---
+++ tests/selenium/login.py
@@ -0,0 +1,30 @@
+import unittest
+from selenium import webdriver
+
+
+class PythonOrgSearch(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+
+    def test_search_in_python_org(self):
+        driver = self.driver
+        driver.get("http://web.engr.oregonstate.edu/~lichlyts/cs561/pages/")
+        self.assertIn("Aristotle", driver.title)
+        login_home_element = driver.find_element_by_class_name("btn")
+        login_home_element.click()
+
+        username_element = driver.find_element_by_id("username")
+        username_element.send_keys("qud")
+        password_element = driver.find_element_by_id("password")
+        password_element.send_keys("Hhrhl1504")
+        login_btn_element = driver.find_element_by_name("_eventId_proceed")
+        login_btn_element.click()
+
+        self.assertIn("pages/ta.php", driver.current_url)
+
+    def tearDown(self):
+        self.driver.quit()
+
+
+if __name__ == "__main__":
+    unittest.main()
