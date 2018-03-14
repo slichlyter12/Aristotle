d0b252b268c6e50fd28da341295a8158daa2ea04
DeqingQu <qu.deqing@gmail.com>
2018-03-13 17:45:42 -0700

    select role page decoration

--- pages/selectrole.php
+++ pages/selectrole.php
@@ -1,15 +1,15 @@
 <?php
-	
+
 	include_once 'db_config/conn.php';
-	
+
 	// set session variables
 	$username = mysqli_real_escape_string($mysqli, strip_tags($_SESSION['onidid']));;
 	$firstname = mysqli_real_escape_string($mysqli, strip_tags($_SESSION['firstname']));;
 	$lastname = mysqli_real_escape_string($mysqli, strip_tags($_SESSION['lastname']));;
-	
+
 	// if form submitted, add user to database and redirect
 	if (isset($_POST['role'])) {
-		
+
 		// sanitize role
 		$role = mysqli_real_escape_string($mysqli, strip_tags($_POST['role']));
 		if ($role == "TA") {
@@ -27,13 +27,13 @@
 		} else {
 			die("Malicious code detected!");
 		}
-		
+
 		// add user to database
 		if ($insertStatement = $mysqli->prepare("INSERT INTO t_user (first_name, last_name, osu_id, role) VALUES (?, ?, ?, ?)")) {
 			$insertStatement->bind_param("sssi", $firstname, $lastname, $username, $roleInt);
 			$success = $insertStatement->execute();
 			$mysqli->close();
-			
+
 			if ($success) {
 				// redirect
 				header("Location: $redirectURL");
@@ -42,23 +42,34 @@
 			}
 		} else {
 			die("Failed to prepare INSERT statement");
-		}		
+		}
 	}
-	
+
 ?>
 <!DOCTYPE html>
 <html>
 	<head>
 		<title>Select Role</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=3">
+		<meta charset="UTF-8">
+		<link rel="stylesheet" href="./css/login_page/style.css">
+		<link rel="stylesheet" href="./css/login_page/home-style.css">
+		<link rel="stylesheet" type="text/css" href="css/base.css" />
 	</head>
 	<body>
-		<h1>Welcome <?php echo $firstname . " " . $lastname; ?>!</h1>
-		<h2>It appears you haven't visited this site before,</h2>
-		<h4>Please select your role below:</h4>
-		<form action="selectrole.php" method="post" accept-charset="utf-8">
-			<input type="submit" name="role" value="TA">
-			<input type="submit" name="role" value="Student">
-			<input type="submit" name="role" value="Admin">
-		</form>
+		<main>
+			<style>@media (min-width:800px) {html { background-image: url(css/login_page/bg/large2); }}</style>
+			<h1 style="margin-left:0rem">Welcome <?php echo $firstname . " " . $lastname; ?>!</h1>
+			<div class="tagline" style="max-width:100in">
+				It appears you haven't visited this site before,
+				<br>
+				Please select your role below:
+			</div>
+			<form action="selectrole.php" method="post" accept-charset="utf-8">
+				<input class='btn' type="submit" name="role" value="TA" style="margin-right: 1%;">
+				<input class='btn' type="submit" name="role" value="Student" style="margin-right: 1%;">
+				<input class='btn' type="submit" name="role" value="Admin">
+			</form>
+		</main>
 	</body>
 </html>
